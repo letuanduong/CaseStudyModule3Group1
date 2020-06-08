0c40ca5d9cf2cc111a76ee741930b2f800e10857
DucMystery <58973644+DucMystery@users.noreply.github.com>
2020-06-08 16:07:23 +0700

    Them phan giao dien cho web

--- Test/model/ProductFunction/ProductServiceImplementTest.java
+++ Test/model/ProductFunction/ProductServiceImplementTest.java
@@ -1,26 +1,26 @@
-package model.ProductFunction;
-
-import static org.junit.jupiter.api.Assertions.*;
-
-class ProductServiceImplementTest {
-
-    @org.junit.jupiter.api.Test
-    void allProduct() {
-    }
-
-    @org.junit.jupiter.api.Test
-    void insertProduct() {
-    }
-
-    @org.junit.jupiter.api.Test
-    void selectProduct() {
-    }
-
-    @org.junit.jupiter.api.Test
-    void deleteProduct() {
-    }
-
-    @org.junit.jupiter.api.Test
-    void updateProduct() {
-    }
-}
+//package model.ProductFunction;
+//
+//import static org.junit.jupiter.api.Assertions.*;
+//
+//class ProductServiceImplementTest {
+//
+//    @org.junit.jupiter.api.Test
+//    void allProduct() {
+//    }
+//
+//    @org.junit.jupiter.api.Test
+//    void insertProduct() {
+//    }
+//
+//    @org.junit.jupiter.api.Test
+//    void selectProduct() {
+//    }
+//
+//    @org.junit.jupiter.api.Test
+//    void deleteProduct() {
+//    }
+//
+//    @org.junit.jupiter.api.Test
+//    void updateProduct() {
+//    }
+//}
