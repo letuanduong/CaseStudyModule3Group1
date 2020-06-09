28fdbe5a8c562160006c7351f67f1b797c76918b
DucMystery <58973644+DucMystery@users.noreply.github.com>
2020-06-09 10:57:43 +0700

    add url databases

--- src/service/DBConnection.java
+++ src/service/DBConnection.java
@@ -8,7 +8,7 @@ public class DBConnection {
     private Connection connection;
     private static DBConnection dbConnection;
     private DBConnection(String username, String password){
-        String url ="jdbc:mysql://localhost:3306/";
+        String url ="jdbc:mysql://localhost:3306/case_study_module3";
         try {
             this.connection = DriverManager.getConnection(url, username, password);
         } catch (SQLException e) {
